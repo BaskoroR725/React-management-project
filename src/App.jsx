883771dac992d2bc2d57c266409a3b9c5e1c532b
Baskoro Ramadhan <baskoror725@gmail.com>
2024-12-10 19:37:10 +0700

    add newproject and input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from './component/NewProject';
 import SideBarProject from './component/SideBar';
 
 function App() {
   return (
-    <main>
+    <main className='h-screen my-8 flex gap-8'>
       <SideBarProject/>
+      <NewProject />
     </main>
   );
 }
