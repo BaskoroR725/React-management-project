139886bcc3f3f712dbf8d4079030e900318720ae
Baskoro Ramadhan <baskoror725@gmail.com>
2024-12-10 21:02:00 +0700

    make a noproject and button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from './component/NewProject';
+import NoProjectSelected from './component/NoProjectSelected';
 import SideBarProject from './component/SideBar';
 
 function App() {
   return (
     <main className='h-screen my-8 flex gap-8'>
       <SideBarProject/>
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
