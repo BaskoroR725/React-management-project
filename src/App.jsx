f6ab0170830b1f2d96d97944da0843052f5607cf
Baskoro Ramadhan <baskoror725@gmail.com>
2024-12-10 21:53:11 +0700

    updated style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react';
 import NewProject from './component/NewProject';
 import NoProjectSelected from './component/NoProjectSelected';
 import SideBarProject from './component/SideBar';
 
 function App() {
+  const [projectState, setprojectState] = useState({
+    selectedProjectId: undefined,
+    project: []
+  });
+
+  function handleStartAddProject() {
+    setprojectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <SideBarProject/>
-      <NoProjectSelected />
+      <SideBarProject onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
